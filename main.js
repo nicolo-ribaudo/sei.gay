e7b1d4ac67ecc5c1eb62abecd7ca71daaf2e70d9
Nicolò Ribaudo <nicolo.ribaudo@gmail.com>
2022-07-03 01:44:37 +0200

    Clamp custom text size

--- main.js
+++ main.js
@@ -16,7 +16,7 @@ const flag = params.get("f");
 if (flag) {
   document.querySelectorAll("[data-template='flag']").forEach(($el) => {
     $el.textContent = `${flag}`;
-    $el.style.fontSize = `min(5em, ${15 / flag.length}em)`;
+    $el.style.fontSize = `clamp(1em, ${15 / flag.length}em, 5em)`;
     try {
       $el.classList.add(`flag-${flag}`);
     } catch {}
